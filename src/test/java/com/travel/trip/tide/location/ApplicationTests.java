f4d8c15a005c811ab9da14fdff05bf4b7421e23d
Dmytro Bekker <dimabekker863@gmail.com>
2023-05-02 23:28:11 +0300

    Adjusted the test

--- src/test/java/com/travel/trip/tide/location/ApplicationTests.java
+++ src/test/java/com/travel/trip/tide/location/ApplicationTests.java
@@ -1,7 +1,11 @@
-package com.travel.Application;
+package com.travel.trip.tide.location;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ApplicationTests {
